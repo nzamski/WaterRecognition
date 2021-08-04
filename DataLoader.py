9046e37e1e5211ec7dc5d36659f9b85907405338
nzamski <nzamski1@gmail.com>
2021-08-04 23:15:13 +0300

    Marking and commenting in DataLoader.py

--- DataLoader.py
+++ DataLoader.py
@@ -52,39 +52,46 @@ class FileLoader:
         self.load_both = load_both
         # source image variables initialization
         self.source_image = load_image(path)
-        self.source_current_x, self.source_current_y = -1, 0
+        self.source_current_x, self.source_current_y = -1, 0  # -1 is a temp starting point
         self.source_max_x = self.source_image.shape[0] - self.length
         self.source_max_y = self.source_image.shape[1] - self.length
         # mask image variables initialization
         self.mask_image = load_y(path) if load_both else None
-        self.mask_current_x = int((length - 1) / 2) - 1 if load_both else None
+        self.mask_current_x = int((length - 1) / 2) - 1 if load_both else None  # -1 is a temp starting point
         self.mask_current_y = int((length - 1) / 2) if load_both else None
         self.mask_max_x = self.mask_image.shape[0] - int((length - 1) / 2) if load_both else None
         self.mask_max_y = self.mask_image.shape[1] - int((length - 1) / 2) if load_both else None
 
     def advance_source_index(self):
         if self.source_current_x < self.source_max_x - self.length:
+            # the slice doesn't exceed the columns
             self.source_current_x += 1
         else:
+            # go to the next row and reset the x
             self.source_current_x = 0
             if self.source_current_y < self.source_max_y - self.length:
                 self.source_current_y += 1
             else:
+                # finished going through image
                 return None
         return self.source_current_x, self.source_current_y
 
     def advance_mask_index(self):
-        if self.mask_current_x < self.mask_max_x - self.length:
+        if self.mask_current_x < self.mask_max_x:  # no need in self.length, right?
+            # the tag doesn't exceed the columns
             self.mask_current_x += 1
         else:
+            # go to the next row and reset the x
             self.mask_current_x = 0
-            if self.mask_current_y < self.mask_max_y - self.length:
+            if self.mask_current_y < self.mask_max_y:  # same
                 self.mask_current_y += 1
             else:
+                # finished going through image
                 return None
         return self.mask_current_x, self.mask_current_y
 
     def get_next(self):
+        # returns the next slice (+ tag)
         source_indices = self.advance_source_index()
         if source_indices is None:
             return None
@@ -127,6 +134,7 @@ class DataLoader:
         return active_file_loaders, file_names
 
     def get_next_loader(self):
+        # return the next file loader (by order) and its path
         if self.current_file_index < len(self.path_list):
             path = self.path_list[self.current_file_index]
             new_loader = FileLoader(path, self.length, self.load_both)
@@ -135,25 +143,33 @@ class DataLoader:
         return None, None
 
     def __next__(self):
+        # initiate lists to store a batch of data
         x_batch, y_batch = list(), list()
         for loader_index, data_loader in enumerate(self.active_file_loaders):
+            # get next potential data loader
             loaded_result = data_loader.get_next()
-            if loaded_result is None:  # end of current loader
+            # end of current loader
+            if loaded_result is None:
                 new_loader, file_name = self.get_next_loader()
-                if new_loader is None:  # finished going through path list
+                # finished going through path list
+                if new_loader is None:
                     del(self.active_file_loaders[loader_index])
                     del(self.file_names[loader_index])
-                    if len(self.active_file_loaders) == 0:  # end of all loaders in active file loaders
+                    # end of all loaders in active file loaders
+                    if len(self.active_file_loaders) == 0:
                         raise StopIteration()
+                # didn't finished going through all the data yet
                 else:
                     data_loader = new_loader
                     self.active_file_loaders[loader_index] = data_loader
                     self.file_names[loader_index] = file_name
                     loaded_result = data_loader.get_next()
+            # finished a batch
             if loaded_result is not None:
                 x_batch.append(loaded_result[0])
                 if self.load_both:
                     y_batch.append(loaded_result[1])
+        # return all x, y batches
         results = [x_batch]
         if self.load_both:
             results.append(y_batch)
